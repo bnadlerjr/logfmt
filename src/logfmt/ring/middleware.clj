906e771d7033e4a646802760ba432e475283aa70
Bob Nadler <bnadlerjr@gmail.com>
2018-01-01 01:28:17 -0500

    Log x-request-id
    If x-request-id is not present in headers, generate one.

--- src/logfmt/ring/middleware.clj
+++ src/logfmt/ring/middleware.clj
@@ -1,10 +1,12 @@
 (ns logfmt.ring.middleware
-  (:require [logfmt.core :refer [info]]))
+  (:require [logfmt.core :refer [info]])
+  (:import (java.util UUID)))
 
 (defn wrap-logger
   [handler]
   (fn [request]
     (let [start (System/currentTimeMillis)
+          request-id (get-in request [:headers "x-request-id"] (UUID/randomUUID))
           method (clojure.string/upper-case (name (:request-method request)))
           path (str (:uri request)
                     (if-let [query-string (:query-string request)]
@@ -12,11 +14,13 @@
       (info "Starting Ring request"
             {:method method
              :path path
-             :params (:params request)})
+             :params (:params request)
+             :request-id request-id})
       (let [response (handler request)]
         (info "Finished Ring request"
               {:method method
                :path path
                :status (:status response)
-               :duration (str (- (System/currentTimeMillis) start) "ms")})
+               :duration (str (- (System/currentTimeMillis) start) "ms")
+               :request-id request-id})
         response))))
