5294fe7db1cdff1e739cd46a40e2c01db277aff8
Bob Nadler <bnadlerjr@gmail.com>
2017-12-31 19:26:26 -0500

    Don't use *out* and *err*; Heroku recommends using System/out for Java

--- src/logfmt/core.clj
+++ src/logfmt/core.clj
@@ -1,9 +1,5 @@
 (ns logfmt.core)
 
-(defn- safe-println [context & more]
-  "The Clojure `println` function has a race condition. See [this blog post](http://yellerapp.com/posts/2014-12-11-14-race-condition-in-clojure-println.html) for more information."
-  (.write context (str (clojure.string/join " " more) "\n")))
-
 (defn- string-with-slash-or-whitespace?
   [s]
   (and (string? s)
@@ -15,12 +11,6 @@
                      "%s=\"%s\""
                      "%s=%s")]
     (format format-str (name k) v)))
-
-(defn- determine-output-context
-  [level]
-  (if (= :info level)
-    *out*
-    *err*))
 
 (defn- full-message
   [dev-mode level message attrs]
@@ -34,9 +24,8 @@
   ([level config message]
    (log level config message {}))
   ([level config message attrs]
-   (let [context (determine-output-context level)
-         full-message (full-message (:dev-mode config) level message attrs)]
-     (safe-println context (clojure.string/trim full-message)))))
+   (let [full-message (full-message (:dev-mode config) level message attrs)]
+     (.println System/out (clojure.string/trim full-message)))))
 
 (def info (partial log :info))
 (def error (partial log :error))
--- test/logfmt/core_test.clj
+++ test/logfmt/core_test.clj
@@ -2,92 +2,96 @@
   (:require [clojure.test :refer :all]
             [logfmt.core :refer :all]))
 
-(defmacro with-err-str
-  "Same behavior as `with-out-str` except this macro binds to *err*
-  instead of *out*."
-  [& body]
-  `(let [s# (new java.io.StringWriter)]
-     (binding [*err* s#]
+(import [java.io ByteArrayOutputStream PrintStream])
+
+(defmacro with-system-out-str [& body]
+  `(let [out-buffer# (ByteArrayOutputStream.)
+         original-out# System/out
+         tmp-out# (PrintStream. out-buffer# true "UTF-8")]
+     (try
+       (System/setOut tmp-out#)
        ~@body
-       (str s#))))
+       (finally
+         (System/setOut original-out#)))
+     (.toString out-buffer# "UTF-8")))
 
 (deftest info-test
   (testing "info message in dev mode"
     (let [config {:dev-mode true}]
       (is (= "info | Some message.\n"
-             (with-out-str (info config "Some message."))))
+             (with-system-out-str (info config "Some message."))))
 
       (is (= "info | A message. duration=10ms\n"
-             (with-out-str (info config "A message." {:duration "10ms"}))))
+             (with-system-out-str (info config "A message." {:duration "10ms"}))))
 
       (is (= "info | My message. duration=12ms method=GET\n"
-             (with-out-str (info config "My message." {:duration "12ms"
+             (with-system-out-str (info config "My message." {:duration "12ms"
                                                        :method "GET"}))))
 
       (is (= "info | Message w/ string attribute. method=POST path=\"/foo\"\n"
-             (with-out-str (info config "Message w/ string attribute." {:method "POST"
+             (with-system-out-str (info config "Message w/ string attribute." {:method "POST"
                                                                         :path "/foo"}))))
 
       (is (= "info | Message w/ nested map attributes. method=GET params={:name \"jdoe\"}\n"
-             (with-out-str (info config "Message w/ nested map attributes." {:method "GET"
+             (with-system-out-str (info config "Message w/ nested map attributes." {:method "GET"
                                                                              :params {:name "jdoe"}}))))))
 
   (testing "info message not in dev mode"
     (let [config {:dev-mode false}]
       (is (= "at=info msg=\"Some message.\"\n"
-             (with-out-str (info config "Some message."))))
+             (with-system-out-str (info config "Some message."))))
 
       (is (= "at=info msg=\"A message.\" duration=10ms\n"
-             (with-out-str (info config "A message." {:duration "10ms"}))))
+             (with-system-out-str (info config "A message." {:duration "10ms"}))))
 
       (is (= "at=info msg=\"My message.\" duration=12ms method=GET\n"
-             (with-out-str (info config "My message." {:duration "12ms"
+             (with-system-out-str (info config "My message." {:duration "12ms"
                                                        :method "GET"}))))
 
       (is (= "at=info msg=\"Message w/ string attribute.\" method=POST path=\"/foo\"\n"
-             (with-out-str (info config "Message w/ string attribute." {:method "POST"
+             (with-system-out-str (info config "Message w/ string attribute." {:method "POST"
                                                                         :path "/foo"}))))
 
       (is (= "at=info msg=\"Message w/ nested map attributes.\" method=GET params={:name \"jdoe\"}\n"
-             (with-out-str (info config "Message w/ nested map attributes." {:method "GET"
+             (with-system-out-str (info config "Message w/ nested map attributes." {:method "GET"
                                                                              :params {:name "jdoe"}})))))))
 
 (deftest error-test
   (testing "error message in dev mode"
     (let [config {:dev-mode true}]
       (is (= "error | Some message.\n"
-             (with-err-str (error config "Some message."))))
+             (with-system-out-str (error config "Some message."))))
 
       (is (= "error | A message. duration=10ms\n"
-             (with-err-str (error config "A message." {:duration "10ms"}))))
+             (with-system-out-str (error config "A message." {:duration "10ms"}))))
 
       (is (= "error | My message. duration=12ms method=GET\n"
-             (with-err-str (error config "My message." {:duration "12ms"
+             (with-system-out-str (error config "My message." {:duration "12ms"
                                                         :method "GET"}))))
 
       (is (= "error | Message w/ string attribute. method=POST path=\"/foo\"\n"
-             (with-err-str (error config "Message w/ string attribute." {:method "POST"
+             (with-system-out-str (error config "Message w/ string attribute." {:method "POST"
                                                                          :path "/foo"}))))
 
       (is (= "error | Message w/ nested map attributes. method=GET params={:name \"jdoe\"}\n"
-             (with-err-str (error config "Message w/ nested map attributes." {:method "GET"
+             (with-system-out-str (error config "Message w/ nested map attributes." {:method "GET"
                                                                               :params {:name "jdoe"}}))))))
   (testing "error message not in dev mode"
     (let [config {:dev-mode false}]
       (is (= "at=error msg=\"Some message.\"\n"
-             (with-err-str (error config "Some message."))))
+             (with-system-out-str (error config "Some message."))))
 
       (is (= "at=error msg=\"A message.\" duration=10ms\n"
-             (with-err-str (error config "A message." {:duration "10ms"}))))
+             (with-system-out-str (error config "A message." {:duration "10ms"}))))
 
       (is (= "at=error msg=\"My message.\" duration=12ms method=GET\n"
-             (with-err-str (error config "My message." {:duration "12ms"
+             (with-system-out-str (error config "My message." {:duration "12ms"
                                                         :method "GET"}))))
 
       (is (= "at=error msg=\"Message w/ string attribute.\" method=POST path=\"/foo\"\n"
-             (with-err-str (error config "Message w/ string attribute." {:method "POST"
+             (with-system-out-str (error config "Message w/ string attribute." {:method "POST"
                                                                          :path "/foo"}))))
 
       (is (= "at=error msg=\"Message w/ nested map attributes.\" method=GET params={:name \"jdoe\"}\n"
-             (with-err-str (error config "Message w/ nested map attributes." {:method "GET"
+             (with-system-out-str (error config "Message w/ nested map attributes." {:method "GET"
                                                                               :params {:name "jdoe"}})))))))
